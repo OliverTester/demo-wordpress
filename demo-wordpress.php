57ca123787cf191807bf7422da24423d66b75b78
olivertester <etimbuk@feefo.com>
2016-03-28 13:08:13 +0100

    added license text to header

--- demo-wordpress.php
+++ demo-wordpress.php
@@ -7,6 +7,18 @@ Version:     1.5
 Author:      Etimbuk Udofia
 Author URI:  http://euedofia.appspot.com
 License:     GPL2
+Demo WordPress is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+Demo WordPress is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Demo WordPress. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Domain Path: /languages
 Text Domain: demo-wordpress
