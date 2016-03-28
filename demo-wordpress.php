8dfaf36968212760e7987d5df292c08472e30ef2
olivertester <etimbuk@feefo.com>
2016-03-28 12:55:17 +0100

    initial wordpress file with headers

---
+++ demo-wordpress.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Demo WordPress
+Plugin URI:  http://euedofia.appspot.com
+Description: This describes my plugin in a short sentence
+Version:     1.5
+Author:      Etimbuk Udofia
+Author URI:  http://euedofia.appspot.com
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Domain Path: /languages
+Text Domain: demo-wordpress
+*/
